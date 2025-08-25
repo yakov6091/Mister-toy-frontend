18b50dabc129c1f71fcf8f24970c74ce9b0cb3c8
Yakov <yakov6091@gmail.com>
2025-08-25 17:15:41 +0300

    Add home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+
+
+export function HomePage() {
+    return (
+        <section className="home-page">
+            <img
+                src=""
+                alt="Toy image" />
+        </section>
+    )
+}
