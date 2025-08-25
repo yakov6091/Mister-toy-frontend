f0432480ee82b339a7d2d24a7fefbed2c912ae55
Yakov <yakov6091@gmail.com>
2025-08-25 17:49:12 +0300

    Add RootCmp with routes

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,2 +1,47 @@
 import { Provider } from 'react-redux'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import './assets/style/main.css'
+
+import { AppFooter } from './cmps/AppFooter'
+import { AppHeader } from './cmps/AppHeader'
+import { HomePage } from './pages/HomePage'
+import { About } from './pages/About'
+import { ToyDetails } from './pages/ToyDetails'
+import { ToyEdit } from './pages/ToyEdit'
+import { ToyIndex } from './pages/ToyIndex'
+
+import { store } from './store/store'
+
+const obj = {
+    className: "main-layout app"
+}
+
+export function App() {
+    return (
+        <Provider store={store}>
+            <Router>
+                <section {...obj}>
+                    <AppHeader />
+
+                    <main>
+                        <Routes>
+                            <Route element={<HomePage />} path="/"></Route>
+                            <Route element={<About />} path="/about"></Route>
+                            <Route element={<ToyIndex />} path="/toy"></Route>
+                            <Route element={<ToyEdit />} path="/toy/edit/:toyId?"></Route>
+                            <Route element={<ToyDetails />} path="/toy/:toyid"></Route>
+                        </Routes>
+                    </main>
+                </section>
+
+            </Router>
+        </Provider>
+    )
+}
+
+
+
+
+
+
 
